369b60706412b2e1161fb3473029ba27be388501
msbrdmr <musaberdemirmc2@gmail.com>
2023-02-14 16:55:12 +0300

    remanaged script files

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+    //this script will run after sv+client is started.
+    //on sv started 
+    //, game will be freezed for each player and then name will be set after button press, 
+    //then game will unfreezed.
+
+    
+}
